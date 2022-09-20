ae969a792a0b9742b57a1becac5d993612c4b22c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-20 12:16:09 +0530

    ADDED: linked list, hash table, my map node, entry class use hash table for calculating fequency of words in sentence

---
+++ src/com/hashtable/MyMapNode.java
@@ -0,0 +1,18 @@
+package com.hashtable;
+
+public class MyMapNode<Key, Value> {
+	public Key key;
+	public Value value;
+	public MyMapNode<Key, Value> next;
+	
+	/**
+	 * Create My Map Node with key value pair data
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	MyMapNode(Key key, Value value) {
+		this.key = key;
+		this.value = value;
+	}
+}
